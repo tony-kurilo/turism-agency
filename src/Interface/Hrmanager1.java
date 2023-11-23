38aecbb0c07c56592a075140ef29cb3867d3e048
Tony Kurilo <lang.nattreise@gmail.com>
2023-11-23 22:51:24 +0200

    add hr mana

--- src/Interface/Hrmanager1.java
+++ src/Interface/Hrmanager1.java
@@ -1,4 +1,26 @@
 package Interface;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Hrmanager1 {
+    @FXML
+    private TableView managersTableView;
+    @FXML
+    private TableColumn loginColumn;
+    @FXML
+    private TableColumn nameColumn;
+    @FXML
+    private TableColumn telNumberColumn;
+    public void scanManagerFile() {
+
+
+    }
 }
--- src/Interface/Login.java
+++ src/Interface/Login.java
@@ -113,11 +113,15 @@ public class Login {
         stage.show();*/
     }
     public void switchToHRManager(javafx.event.ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("hrmanager1.fxml"));
-        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        scene = new Scene (root);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hrmanager1.fxml"));
+        Parent root = loader.load();
+        Hrmanager1 hrmanager1Controller = loader.getController();
+        hrmanager1Controller.refreshButton();
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
     }
 }
 
